955e287167c1de0e07d2dba103821effc54b0e79
koushik_2004 <perumallajaikoushik@gmail.com>
2025-09-05 21:11:47 +0530

    Trying Get methods

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+k={1: "one", 2: "two",3: "three",4: "four",5: "five"}
+
 @app.get("/")
 def read_root():
     return {"First": "API"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str =""):
+    q=k[item_id]
+    return {"item_id": item_id, "q": q}
